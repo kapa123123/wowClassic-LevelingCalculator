29f7c670020543592710eec07da7d738265656c6
Taylor English <s5118418@bournemouth.ac.uk>
2021-03-29 14:13:11 +0100

    Created a wow classic leveling calculator

--- wowClassicXPCalculator/wowClassicXPCalculator.cpp
+++ wowClassicXPCalculator/wowClassicXPCalculator.cpp
@@ -1,20 +1,39 @@
-// wowClassicXPCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iostream>
+
+void input();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	input();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void input()
+{
+	int currXP; //Users Current XP
+	int highestXP; //XP Needed to Level
+	int xpPerKill; //XP Gained farming individual Mob
+	int mobCount; //How many mobs need to be killed to ding
+	
+	std::cout << "Please Input your current XP " << "\n" << ">>";
+	std::cin >> currXP;
+
+	std::cout << "Please Input your Needed XP" << "\n" << ">>";
+	std::cin >> highestXP;
+
+	system("CLS");
+
+	std::cout << "Please Input your XP Per Kill " << "\n" << ">>";
+	std::cin >> xpPerKill;
+
+	system("CLS");
+
+	int neededXP = highestXP - currXP;
+	mobCount = neededXP / xpPerKill;
+
+	std::cout << mobCount;
+	
+
+
+};
